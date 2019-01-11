8fc3247dec7fcf7b37e2983038cb30d796503150
Marc Autord <marc.autord@haarukka.net>
2019-01-11 15:15:16 +0100

    Ajout du requêtage de la base de données

--- temperature.js
+++ temperature.js
@@ -8,12 +8,60 @@ function execute(command, callback){
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
+
+
+
+function getLastDaysTemperatures(nb_days, cb){
+        // Création de la connexion ici car ne peut servir qu'une seule fois
+        var con = mysql.createConnection({
+                host: "localhost",
+                user: "rasp",
+                password: "altUnsyint"
+        })
+
+        con.connect(function(err){
+                if (err) throw err;
+                console.log("Connected to database!");
+                con.query("select DATE_FORMAT(DATE_METER, '%Y-%m-%d %H:%i') as DATE_METER, TEMPERATURE from RASP.TEMPERATURE where DATE_METER > date(now() - interval " + nb_days + " day);", function(err, result){
+                        if (err) throw err;
+                        return cb(null, result)
+                })
+        })
+}
+
+
+
+
+app.get('/getTemperatures', function(req,res){
+        let nb_days =7
+        getLastDayTemperatures(nb_days, (err, dataTemp) => {
+                if (dataTemp){
+                        // return res.json(dataTemp)
+                        return res.send(JSON.stringify(dataTemp))
+                } else {
+                        return null
+                }
+            })
+})
+
+
 app.get('/', (request, response) => {
     console.log("Connexion entrante.")
     execute("python /home/pi/temperatureToDB.py 14", (temperature) => {
 	response.send(JSON.stringify({'temperature': temperature}))
     })
 })
+
+
+// Error Handler for 404 Pages
+app.use(function(req, res, next) {
+        console.log(req)
+        var error404 = new Error('Route Not Found');
+        error404.status = 404;
+        next(error404);
+});
+
+module.exports = app;
 
 app.listen(port, (err) => {
     if (err) {
